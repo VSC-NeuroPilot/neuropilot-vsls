18040451aeeb28c2dca96caab70b716c9c74d074
KTrain5369 <me@git.ktrain5369.dev>
2026-07-27 20:55:12 +1000

    bump @vsc-neuropilot/vsls-redux

--- src/globals.ts
+++ src/globals.ts
@@ -1,6 +1,8 @@
 import { NeuroPilotAPI, CompanionAPI } from "@vsc-neuropilot/api-types";
+import { createVSLS } from "@vsc-neuropilot/vsls-redux";
 
 export class Globals {
     static api: NeuroPilotAPI;
     static companion: CompanionAPI;
+    static vslsStore = createVSLS();
 }
